728bf51f7d8a86161b44fb7125f0acf9ce20b5eb
Jeff Cameron <jeffgcameron@gmail.com>
2023-03-15 16:58:48 -0600

    adds console logs

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 function onSignIn(googleUser) {
     var profile = googleUser.getBasicProfile();
+    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
     $('#name').text(profile.getName());
     $('#email').text(profile.getEmail());
     $('#image').text(profile.getImageUrl());
     $('.data').css('display', 'block');
     $('.g-signin2').css('display', 'none');
-    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-    console.log('Name: ' + profile.getName());
-    console.log('Image URL: ' + profile.getImageUrl());
-    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
   }
 
   function signOut() {
