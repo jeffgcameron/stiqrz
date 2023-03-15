302a434270c3b819caf0336b2a3510eaf700333f
Jeff Cameron <jeffgcameron@gmail.com>
2023-03-15 16:50:40 -0600

    adds console logs

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ function onSignIn(googleUser) {
     $('#image').text(profile.getImageUrl());
     $('.data').css('display', 'block');
     $('.g-signin2').css('display', 'none');
-    // console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-    // console.log('Name: ' + profile.getName());
-    // console.log('Image URL: ' + profile.getImageUrl());
-    // console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
   }
 
   function signOut() {
